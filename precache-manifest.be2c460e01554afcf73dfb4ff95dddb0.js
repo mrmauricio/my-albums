39fa36b590da2604b71d265f4ef1c837ac93785b
mauricio-rossi <mendes-mauricio@outlook.com>
2019-05-18 17:36:40 +0100

    Updates

---
+++ precache-manifest.be2c460e01554afcf73dfb4ff95dddb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02927eaca6c99a0445b687937c9e7ca0",
+    "url": "/my-albums/index.html"
+  },
+  {
+    "revision": "bd1a71e43ecd9b52dfcd",
+    "url": "/my-albums/static/css/2.6466deae.chunk.css"
+  },
+  {
+    "revision": "6a6a785fc1d111e634a4",
+    "url": "/my-albums/static/css/main.864d2bae.chunk.css"
+  },
+  {
+    "revision": "bd1a71e43ecd9b52dfcd",
+    "url": "/my-albums/static/js/2.6758b5e5.chunk.js"
+  },
+  {
+    "revision": "6a6a785fc1d111e634a4",
+    "url": "/my-albums/static/js/main.1c1bbbb2.chunk.js"
+  },
+  {
+    "revision": "16d900ee7ddd5351cb24",
+    "url": "/my-albums/static/js/runtime~main.5d191846.js"
+  },
+  {
+    "revision": "7ce43fc4de0284541b957bd5f83400fa",
+    "url": "/my-albums/static/media/background.7ce43fc4.png"
+  },
+  {
+    "revision": "e99d46f920726a5b61d65eb719eb284c",
+    "url": "/my-albums/static/media/logo.e99d46f9.png"
+  }
+]);
